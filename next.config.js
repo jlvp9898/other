84b4a2964e0bad75a25c194bf496c805265903d1
jlvp9898 <68341886+jlvp9898@users.noreply.github.com>
2022-05-24 10:20:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({});
 module.exports = {
   images: {
-    domains: ['gorros.net', 'scontent.cdninstagram.com', 'scontent-mia3-1.cdninstagram.com', 'scontent-mia3-2.cdninstagram.com', 'scontent-mia3-3.cdninstagram.com', 'video-mia3-1.cdninstagram.com', 'video-mia3-2.cdninstagram.com', 'scontent-mad1-1.cdninstagram.com', 'scontent-mad1-2.cdninstagram.com', 'video-mad1-1.cdninstagram.com', 'video-mad1-2.cdninstagram.com', 'scontent-bos3-1.cdninstagram.com'],
+    domains: ['delorean.billregulator.com', 'scontent.cdninstagram.com', 'scontent-mia3-1.cdninstagram.com', 'scontent-mia3-2.cdninstagram.com', 'scontent-mia3-3.cdninstagram.com', 'video-mia3-1.cdninstagram.com', 'video-mia3-2.cdninstagram.com', 'scontent-mad1-1.cdninstagram.com', 'scontent-mad1-2.cdninstagram.com', 'video-mad1-1.cdninstagram.com', 'video-mad1-2.cdninstagram.com', 'scontent-bos3-1.cdninstagram.com'],
   },
   
 }
